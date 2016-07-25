fe45896c94f11977686224059ab7e76ccbd80f12
THN-Dong <609042238@qq.com>
2016-07-25 13:58:48 +0800

    添加了忽略的内容

---
+++ Fifish/Fifish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fifish
+//
+//  Created by THN-Dong on 16/7/25.
+//  Copyright © 2016年 Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
