0d492dfcadeedce88d0eaa6b2b6ac0a9958a7a76
THN-Dong <609042238@qq.com>
2016-08-03 19:36:00 +0800

    no message

--- Fifish/Fifish/Classes/Me/Model/FSZuoPin.h
+++ Fifish/Fifish/Classes/Me/Model/FSZuoPin.h
@@ -8,9 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum {
+    FSZuoPinTypePicture = 1,
+    FSZuoPinTypeVideo = 11
+} FSZuoPinType;
+
 @interface FSZuoPin : NSObject
 
 /** cell的高度 */
 @property (nonatomic, assign, readonly) CGFloat cellHeight;
-
+/** 帖子的类型 */
+@property (nonatomic, assign) FSZuoPinType type;
+/** 图片控件的frame */
+@property (nonatomic, assign, readonly) CGRect pictureF;
 @end
--- Fifish/Fifish/Classes/Me/Model/FSZuoPin.m
+++ Fifish/Fifish/Classes/Me/Model/FSZuoPin.m
@@ -7,6 +7,7 @@
 //
 
 #import "FSZuoPin.h"
+#import "FSConst.h"
 
 @implementation FSZuoPin
 
@@ -16,9 +17,29 @@
 
 //-(CGFloat)cellHeight{
 //    if (!_cellHeight) {
-//        _cellHeight = <#创建成员属性#>;
+//        if (self.type == FSZuoPinTypePicture) { // 图片
+//            // 图片显示出来的宽度
+//            CGFloat pictureW = SCREEN_WIDTH;
+//            // 显示显示出来的高度
+//            CGFloat pictureH = pictureW * SCREEN_HEIGHT / SCREEN_WIDTH;
+//            
+//            // 计算图片控件的frame
+//            CGFloat pictureX = 0;
+//            CGFloat pictureY = 10;
+//            _pictureF = CGRectMake(pictureX, pictureY, pictureW, pictureH);
+//            
+//            _cellHeight += pictureH + XMGTopicCellMargin;
+//        } else if (self.type == XMGTopicTypeVoice) { // 声音帖子
+//            CGFloat voiceX = XMGTopicCellMargin;
+//            CGFloat voiceY = XMGTopicCellTextY + textH + XMGTopicCellMargin;
+//            CGFloat voiceW = maxSize.width;
+//            CGFloat voiceH = voiceW * self.height / self.width;
+//            _voiceF = CGRectMake(voiceX, voiceY, voiceW, voiceH);
+//            
+//            _cellHeight += voiceH + XMGTopicCellMargin;
+//        }
 //    }
-//    return <#成员属性#>;
+//    return _cellHeight;
 //}
 
 @end
