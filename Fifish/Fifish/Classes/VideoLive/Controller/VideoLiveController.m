55bcd6dbe641cb28ed426dd2860ae9a28a3421f1
MC <41363622@qq.com>
2016-10-18 22:07:08 +0800

    测试地址变更

--- Fifish/Fifish/Classes/VideoLive/Controller/VideoLiveController.m
+++ Fifish/Fifish/Classes/VideoLive/Controller/VideoLiveController.m
@@ -243,9 +243,9 @@
 - (FifishH264Decoder *)ViedoDecoder{
     if (!_ViedoDecoder) {
         //rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp
-//        _ViedoDecoder  = [[FifishH264Decoder alloc] initWithUrl:@"rtsp://admin:admin@192.168.2.158:554/channel1/2"];
+        _ViedoDecoder  = [[FifishH264Decoder alloc] initWithUrl:@"rtsp://admin:admin@192.168.2.158:554/channel1/2"];
 //        _ViedoDecoder  = [[FifishH264Decoder alloc] initWithUrl:@"http://pull99.a8.com/live/1476687770278362.flv"];
-        _ViedoDecoder  = [[FifishH264Decoder alloc] initWithUrl:@"http://pull99.a8.com/live/1476690710257396.flv"];
+//        _ViedoDecoder  = [[FifishH264Decoder alloc] initWithUrl:@"http://pull99.a8.com/live/1476690710257396.flv"];
 //        _ViedoDecoder  = [[FifishH264Decoder alloc] initWithUrl:@"rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp"];
         _ViedoDecoder.UpdataDelegate = self;
     }
